b115e5eb243b5ccded721cbc59e27c6971b14c94
xulder <sebastianjansen04@gmail.com>
2024-09-06 08:23:23 -0700

    feat(cpu.rs/opcode.rs): finished opcode rewrite

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,14 +1,12 @@
-use core::panic;
-
 use crate::memory::Memory;
-use crate::opcode::Opcode;
-use crate::registers::{pack_registers, unpack_registers, Register, RegisterFile};
+use crate::opcode::{Opcode, Instruction, Mode};
+use crate::registers::{unpack_registers, Register, RegisterFile};
 
 /// The CPU of the Tiny Computer
 #[derive(Debug)]
 pub struct Cpu<const M: usize> {
     /// The registers of the CPU
-    pub registers: RegisterFile,
+    registers: RegisterFile,
 }
 
 impl<const M: usize> Cpu<M> {
@@ -37,8 +35,18 @@ impl<const M: usize> Cpu<M> {
     }
 
     #[inline]
+    pub fn set_register_pair(&mut self, register: Register, value: u16) {
+        self.registers.set_reg_pair(register, value)
+    }
+
+    #[inline]
     pub fn get_register(&self, register: Register) -> u8 {
         self.registers.get_reg(register)
+    }
+
+    #[inline]
+    pub fn get_register_pair(&self, register: Register) -> u16 {
+        self.registers.get_reg_pair(register)
     }
 
     pub fn get_sptr(&self) -> u16 {
@@ -63,7 +71,7 @@ impl<const M: usize> Cpu<M> {
     /// # Arguments
     ///
     /// * `memory` - The memory to consume from
-    pub fn consume(&mut self, memory: &Memory<M>) -> u8 {
+    pub fn consume_u8(&mut self, memory: &Memory<M>) -> u8 {
         let value = memory.data[self.registers.pc as usize];
         self.registers.pc += 1;
         value
@@ -81,75 +89,141 @@ impl<const M: usize> Cpu<M> {
         value
     }
 
-    #[inline]
-    fn consume_imm8(&mut self, memory: &Memory<M>) -> (Register, u8, u8) {
-        let dest_register = Register::from(self.consume(memory));
-        let dest_value = self.registers.get_reg(dest_register);
-        let value = self.consume(memory);
-        (dest_register, dest_value, value)
-    }
-
-    #[inline]
-    fn consume_imm16(&mut self, memory: &Memory<M>) -> (Register, u16, u16) {
-        let dest_register = Register::from(self.consume(memory));
-        let dest_value = self.registers.get_reg_pair(dest_register);
-        let value = self.consume_u16(memory);
-        (dest_register, dest_value, value)
-    }
-
-    #[inline]
-    fn consume_reg_dir(&mut self, memory: &Memory<M>) -> (Register, u8, u8) {
-        // convert these into Register
-        let (dest_register, src_register) = unpack_registers(self.consume(memory));
-        let dest_value = self.registers.get_reg(dest_register.into());
-        let src_value = self.registers.get_reg(src_register.into());
-        (dest_register.into(), dest_value, src_value)
-    }
-
-    #[inline]
-    fn consume_reg(&mut self, memory: &Memory<M>) -> (Register, u8, u8) {
-        let (dest_register, src_register) = unpack_registers(self.consume(memory));
-        let dest_value = self.registers.get_reg(dest_register.into());
-        let value_addr = self.registers.get_reg_pair(src_register.into());
-        let value = memory.read_mem_u8(value_addr).unwrap();
-        (dest_register.into(), dest_value, value)
-    }
-
-    #[inline]
-    fn consume_reg_ind_x(&mut self, memory: &Memory<M>) -> (Register, u8, u8) {
-        let (dest_register, src_register) = unpack_registers(self.consume(memory));
-        let offset = self.consume(memory);
-        let dest_value = self.registers.get_reg(dest_register.into());
-        let value_addr = self.registers.get_reg_pair(src_register.into()).wrapping_add(offset as u16);
-        let value = memory.read_mem_u8(value_addr).unwrap();
-        (dest_register.into(), dest_value, value)
-    }
-
-    #[inline]
-    fn consume_dir(&mut self, memory: &Memory<M>) -> (Register, u8, u8) {
-        let dest_register = Register::from(self.consume(memory));
-        let dest_value = self.registers.get_reg(dest_register);
-        let addr = self.consume_u16(memory);
-        let value = memory.read_mem_u8(addr).unwrap();
-        (dest_register, dest_value, value)
-    }
-
     /// Executes a single instruction
     ///
     /// # Arguments
     ///
     /// * `memory` - The memory to execute the instruction on
     pub fn execute(&mut self, memory: &mut Memory<M>) {
-        let opcode = Opcode::from(self.consume(memory));
-        match opcode {
-            Opcode::Nop => {}
+        let opcode = Opcode::from(self.consume_u8(memory));
+        let instruction = opcode.instruction();
+        let mode = opcode.mode();
+        match instruction {
+            Instruction::Load => {
+                match mode {
+                    Mode::Immediate => {
+                        let dest_reg = Register::from(self.consume_u8(memory));
+                        if dest_reg.is_pair() {
+                            let value = self.consume_u16(memory);
+                            self.set_register_pair(dest_reg, value);
+                        } else {
+                            let value = self.consume_u8(memory);
+                            self.set_register(dest_reg, value);
+                        }
+                    }
+                    Mode::Register => {
+                        let (dest_reg, src_reg) = unpack_registers(self.consume_u8(memory));
+                        if dest_reg.is_pair() {
+                            let value = self.get_register_pair(src_reg);
+                            self.set_register_pair(dest_reg, value);
+                        } else {
+                            let value = self.get_register(src_reg);
+                            self.set_register(dest_reg, value);
+                        }
+                    }
+                    Mode::RegisterIndirect => {
+                        let (dest_reg, src_reg) = unpack_registers(self.consume_u8(memory));
+                        if dest_reg.is_pair() {
+                            let value_addr = self.get_register_pair(src_reg);
+                            let value = memory.read_mem_u16(value_addr).unwrap();
+                            self.set_register_pair(dest_reg, value);
+                        } else {
+                            let value_addr = self.get_register_pair(src_reg);
+                            let value = memory.read_mem_u8(value_addr).unwrap();
+                            self.set_register(dest_reg, value);
+                        }
+                    }
+                    Mode::RegisterIndexed => {
+                        let (dest_reg, src_reg) = unpack_registers(self.consume_u8(memory));
+                        let offset = self.consume_u16(memory);
+                        if dest_reg.is_pair() {
+                            let value_addr = self.get_register_pair(src_reg) + offset;
+                            let value = memory.read_mem_u16(value_addr).unwrap();
+                            self.set_register_pair(dest_reg, value);
+                        } else {
+                            let value_addr = self.get_register_pair(src_reg) + offset;
+                            let value = memory.read_mem_u8(value_addr).unwrap();
+                            self.set_register(dest_reg, value);
+                        }
+                    }
+                    Mode::Direct => {
+                        let dest_reg = Register::from(self.consume_u8(memory));
+                        let addr = self.consume_u16(memory);
+                        if dest_reg.is_pair() {
+                            let value = memory.read_mem_u16(addr).unwrap();
+                            self.set_register_pair(dest_reg, value);
+                        } else {
+                            let value = memory.read_mem_u8(addr).unwrap();
+                            self.set_register(dest_reg, value);
+                        }
+                    }
+                    _ => unimplemented!(),
+                }
+            },
+            Instruction::Write => {
+                match mode {
+                    Mode::Direct => {
+                        let dest_reg = Register::from(self.consume_u8(memory));
+                        let addr = self.consume_u16(memory);
+                        if dest_reg.is_pair() {
+                            let value = self.get_register_pair(dest_reg);
+                            memory.write_mem_u16(addr, value).unwrap();
+                        } else {
+                            let value = self.get_register(dest_reg);
+                            memory.write_mem_u8(addr, value).unwrap();
+                        }
+                    }
+                    _ => unimplemented!(),
+                }
+            }
             _ => unimplemented!(),
         }
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::opcode::*;
-//     use super::*;
-// }
+#[cfg(test)]
+mod tests {
+    use crate::registers::pack_registers;
+
+    use super::*;
+
+    const M: usize = 56;
+
+    #[test]
+    pub fn test_load_imm() {
+        let mut cpu = Cpu::<M>::new();
+        let mut memory = Memory::<M>::new();
+
+        let load_imm = Opcode::new(Instruction::Load, Mode::Immediate);
+
+        // writing test program to memory
+        // load a 1
+        memory.write_mem_u8(0, load_imm.as_u8()).unwrap();
+        memory.write_mem_u8(1, Register::A.as_u8()).unwrap();
+        memory.write_mem_u8(2, 1).unwrap();
+
+        cpu.execute(&mut memory);
+
+        assert_eq!(cpu.get_register(Register::A), 1);
+    }
+
+    #[test]
+    fn test_load_reg() {
+        let mut cpu = Cpu::<M>::new();
+        let mut memory = Memory::<M>::new();
+
+        cpu.set_register(Register::A, 0);
+        cpu.set_register(Register::B, 1);
+    
+        let load_reg = Opcode::new(Instruction::Load, Mode::Register);
+        let a_and_b = pack_registers(Register::A.as_u8(), Register::B.as_u8());
+    
+        // load a b
+        memory.write_mem_u8(0, load_reg.as_u8()).unwrap();
+        memory.write_mem_u8(1, a_and_b).unwrap();
+    
+        cpu.execute(&mut memory);
+    
+        assert_eq!(cpu.get_register(Register::A), 1);
+    }
+}
--- src/opcode.rs
+++ src/opcode.rs
@@ -1,151 +1,210 @@
-/// An enum representing all possible opcodes.
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
-pub enum Opcode {
-    // TODO: change to hex format
+pub enum Mode {
+    /// op, reg, (imm8/imm16)
+    Immediate = 0b00,
+    /// op reg reg
+    Register = 0b01,
+    /// op reg [reg]
+    RegisterIndirect = 0b10,
+    /// op reg [reg + imm16]
+    RegisterIndexed = 0b11,
+    /// op reg [imm16]
+    Direct = 0b100,
+    /// jmp label|offset|reg|(imm8/imm16)
+    Relative = 0b101,
+}
+
+impl From<Mode> for u8 {
+    /// Converts a `Mode` enum to an `u8` value.
+    fn from(value: Mode) -> Self {
+        value as u8
+    }
+}
+
+impl From<u8> for Mode {
+    /// Converts an `u8` value to a `Mode` enum.
+    fn from(value: u8) -> Self {
+        unsafe { std::mem::transmute(value) }
+    }
+}
+
+
+impl std::fmt::Display for Mode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Mode::Immediate => write!(f, "Immediate"),
+            Mode::Register => write!(f, "Register"),
+            Mode::RegisterIndirect => write!(f, "RegisterIndirect"),
+            Mode::RegisterIndexed => write!(f, "RegisterIndexed"),
+            Mode::Direct => write!(f, "Direct"),
+            Mode::Relative => write!(f, "Relative"),
+        }
+    }
+}
+
+/// An enum representing all possible instructions.
+/// # Immediate
+/// `op reg8/reg16 imm8/imm16`
+/// 
+/// # Register
+/// `op reg8/reg16 reg8/reg16`
+/// 
+/// # RegisterIndirect
+/// `op reg8/reg16 [reg16]`
+/// 
+/// # RegisterIndexed
+/// `op reg8/reg16 [reg16 + imm16]`
+/// 
+/// # Direct
+/// `op reg8/reg16 [imm16]`
+/// 
+/// # Relative
+/// `jmp label|offset|reg|(imm8/imm16)`
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Instruction {
     /// No operation.
     Nop = 0x00,
 
-    /// add a, 2 -> adds 2 to register a
-    AddImm8 = 0x01,
-    AddImm16 = 0x02,
-    AddRegDir = 0x03,
-    AddReg = 0x04,
-    AddDir = 0x05,
+    /// Halt the CPU.
 
-    /// sub a, 2 -> subtracts 2 from register a
-    SubImm8 = 0x06,
-    /// sub ab, [0x1111] -> subtracts 0x1111 from registers a and b treated as a big endian pair.
-    SubImm16 = 0x07,
-    /// sub a, [bc] -> subtracts the value at address contained in bc from register a
-    SubRegDir = 0x08,
-    /// sub a, [bc + 1] -> subtracts the value at address contained in bc plus 1 from register a
-    SubRegInd = 0x09,
-    /// sub a, [x05] -> subtracts the value at address [x05] from register a
-    SubDir = 0x0A,
+    Add = 0x01,
 
-    /// mul a, 2 -> multiplies register a by 2
-    MulImm8 = 0x0B,
-    /// mul ab, [0x1111] -> multiplies registers a and b treated as a big endian pair by 0x1111
-    MulImm16 = 0x0C,
-    /// mul a, [bc] -> multiplies register a by the value at address contained in bc
-    MulRegDir = 0x0D,
-    /// mul a, [bc + 1] -> multiplies register a by the value at address contained in bc plus 1
-    MulRegInd = 0x0E,
-    /// mul a, [x05] -> multiplies register a by the value at address [x05]
-    MulDir = 0x0F,
+    Sub = 0x02,
 
-    /// div a, 2 -> divides register a by 2
-    DivImm8 = 0x10,
-    /// div ab, [0x1111] -> divides registers a and b treated as a big endian pair by 0x1111
-    DivImm16 = 0x11,
-    /// div a, [bc] -> divides register a by the value at address contained in bc
-    DivRegDir = 0x12,
-    /// div a, [bc + 1] -> divides register a by the value at address contained in bc plus 1
-    DivRegInd = 0x13,
-    /// div a, [x05] -> divides register a by the value at address [x05]
-    DivDir = 0x14,
+    Mul = 0x03,
 
-    /// mov a, 2 -> moves the value 2 to register a
-    MovImm8 = 0x15,
-    /// mov a, [bc] -> moves the value at address contained in bc to register a
-    MovRegDir = 0x16,
-    /// mov a, [bc + 1] -> moves the value at address contained in bc plus 1 to register a
-    MovRegInd = 0x17,
-    /// mov a, [x05] -> moves the value at address [x05] to register a
-    MovDir = 0x18,
+    Div = 0x04,
 
-    /// psh a, 2 -> pushes the value 2 to the stack pointed to by register a
-    PshImm8 = 0x19,
-    /// psh a, [bc] -> pushes the value at address contained in bc to the stack pointed to by register a
-    PshRegDir = 0x1A,
-    /// psh a, [bc + 1] -> pushes the value at address contained in bc plus 1 to the stack pointed to by register a
-    PshRegInd = 0x1B,
-    /// psh a, [x05] -> pushes the value at address [x05] to the stack pointed to by register a
-    PshDir = 0x1C,
+    Mod = 0x05,
 
-    /// pop a -> pops the top value off the stack and moves it to register a
-    Pop = 0x1D,
+    And = 0x06,
 
-    /// wrt a, 2 -> writes the value 2 to the memory at the address contained in register a
-    WrtImm8 = 0x1E,
-    /// wrt a, [bc] -> writes the value at address contained in bc to the memory at the address contained in register a
-    WrtRegDir = 0x1F,
-    /// wrt a, [bc + 1] -> writes the value at address contained in bc plus 1 to the memory at the address contained in register a
-    WrtRegInd = 0x20,
+    Or = 0x07,
 
-    /// cmp a, 2 -> compares the value 2 to register a
-    CmpImm8 = 0x21,
-    /// cmp a, [bc] -> compares the value at address contained in bc to register a
-    CmpRegDir = 0x22,
-    /// cmp a, [bc + 1] -> compares the value at address contained in bc plus 1 to register a
-    CmpRegInd = 0x23,
-    /// cmp a, [x05] -> compares the value at address [x05] to register a
-    CmpDir = 0x24,
+    Xor = 0x08,
 
-    /// jmp [x05] -> jumps to the address [x05]
-    Jmp = 0x25,
-    /// jz [x05] -> jumps to the address [x05] if the zero flag is set
-    Jz = 0x26,
-    /// jnz [x05] -> jumps to the address [x05] if the zero flag is not set
-    Jnz = 0x27,
+    Not = 0x09,
 
-    /// hlt -> halts the processor
-    Hlt = 0x28,
+    Lsh = 0x0a,
+
+    Rsh = 0x0b,
+
+    Cmp = 0x0c,
+
+    Jmp = 0x0d,
+
+    Jmpf = 0x0e,
+
+    Jmpb = 0x0f,
+
+    Call = 0x10,
+
+    Ret = 0x11,
+
+    Push = 0x12,
+
+    Pop = 0x13,
+
+    Write = 0x14,
+
+    Load = 0x15,
+
+    In = 0x16,
+
+    Out = 0x17,
+
+    Halt = 0x1F,
+
 }
 
-impl From<u8> for Opcode {
+impl From<Instruction> for u8 {
+    /// Converts an `Opcode` enum to an `u8` value.
+    fn from(value: Instruction) -> Self {
+        value as u8
+    }
+}
+
+impl From<u8> for Instruction {
+    /// Converts an `u8` value to a `Opcode` enum.
     fn from(value: u8) -> Self {
-        match value {
-            0x00 => Opcode::Nop,
-            0x01 => Opcode::AddImm8,
-            0x02 => Opcode::AddImm16,
-            0x03 => Opcode::AddRegDir,
-            0x04 => Opcode::AddReg,
-            0x05 => Opcode::AddDir,
-            0x06 => Opcode::SubImm8,
-            0x07 => Opcode::SubImm16,
-            0x08 => Opcode::SubRegDir,
-            0x09 => Opcode::SubRegInd,
-            0x0A => Opcode::SubDir,
-            0x0B => Opcode::MulImm8,
-            0x0C => Opcode::MulImm16,
-            0x0D => Opcode::MulRegDir,
-            0x0E => Opcode::MulRegInd,
-            0x0F => Opcode::MulDir,
-            0x10 => Opcode::DivImm8,
-            0x11 => Opcode::DivImm16,
-            0x12 => Opcode::DivRegDir,
-            0x13 => Opcode::DivRegInd,
-            0x14 => Opcode::DivDir,
-            0x15 => Opcode::MovImm8,
-            0x16 => Opcode::MovRegDir,
-            0x17 => Opcode::MovRegInd,
-            0x18 => Opcode::MovDir,
-            0x19 => Opcode::PshImm8,
-            0x1A => Opcode::PshRegDir,
-            0x1B => Opcode::PshRegInd,
-            0x1C => Opcode::PshDir,
-            0x1D => Opcode::Pop,
-            0x1E => Opcode::WrtImm8,
-            0x1F => Opcode::WrtRegDir,
-            0x20 => Opcode::WrtRegInd,
-            0x21 => Opcode::CmpImm8,
-            0x22 => Opcode::CmpRegDir,
-            0x23 => Opcode::CmpRegInd,
-            0x24 => Opcode::CmpDir,
-            0x25 => Opcode::Jmp,
-            0x26 => Opcode::Jz,
-            0x27 => Opcode::Jnz,
-            0x28 => Opcode::Hlt,
-            _ => panic!("Invalid opcode."),
+        unsafe { std::mem::transmute(value) }
+    }
+}
+
+impl std::fmt::Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            // TODO: Lowercase these
+            Instruction::Nop => write!(f, "Nop"),
+            Instruction::Add => write!(f, "Add"),
+            Instruction::Sub => write!(f, "Sub"),
+            Instruction::Mul => write!(f, "Mul"),
+            Instruction::Div => write!(f, "Div"),
+            Instruction::Mod => write!(f, "Mod"),
+            Instruction::And => write!(f, "And"),
+            Instruction::Or => write!(f, "Or"),
+            Instruction::Xor => write!(f, "Xor"),
+            Instruction::Not => write!(f, "Not"),
+            Instruction::Lsh => write!(f, "Lsh"),
+            Instruction::Rsh => write!(f, "Rsh"),
+            Instruction::Cmp => write!(f, "Cmp"),
+            Instruction::Jmp => write!(f, "Jmp"),
+            Instruction::Jmpf => write!(f, "Jmpf"),
+            Instruction::Jmpb => write!(f, "Jmpb"),
+            Instruction::Push => write!(f, "Push"),
+            Instruction::Pop => write!(f, "Pop"),
+            Instruction::Call => write!(f, "Call"),
+            Instruction::Ret => write!(f, "Ret"),
+            Instruction::Write => write!(f, "Store"),
+            Instruction::Load => write!(f, "Load"),
+            Instruction::In => write!(f, "In"),
+            Instruction::Out => write!(f, "Out"),
+            Instruction::Halt => write!(f, "Halt"),
         }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct Opcode(u8);
+
+impl Opcode {
+
+    pub fn new(instruction: Instruction, mode: Mode) -> Opcode {
+        Opcode(((instruction as u8) << 3) | (mode as u8))
+    }
+
+    pub fn instruction(&self) -> Instruction {
+        Instruction::from(self.0 >> 3)
+    }
+
+    pub fn mode(&self) -> Mode {
+        Mode::from(self.0 & 0b111)
+    }
+
+    pub fn as_u8(&self) -> u8 {
+        self.0
     }
 }
 
 impl From<Opcode> for u8 {
     fn from(value: Opcode) -> Self {
-        value as u8
+        value.0
+    }
+}
+
+impl From<u8> for Opcode {
+    fn from(value: u8) -> Self {
+        Opcode(value)
+    }
+}
+
+impl std::fmt::Display for Opcode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.instruction(), self.mode())
     }
 }
 
